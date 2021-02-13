855de3969bf646ea44d579c1222ca91a02ee7809
Serkan Yılmaz <seerkanyilmazz@gmail.com>
2021-02-13 16:32:12 +0300

    trying to learn android widgets
    checkbox example added

--- androidWidgets/app/src/main/java/com/seerkanyilmazz/androidwidgets/MainActivity.java
+++ androidWidgets/app/src/main/java/com/seerkanyilmazz/androidwidgets/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -23,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private ToggleButton toggleButton2;
     private Button submitButton;
 
+    //checkbox_exampl.xml
+    private CheckBox coffee;
+    private CheckBox pizza;
+    private CheckBox burger;
+    private Button orderButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -32,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        setContentView(R.layout.button_example);
 //        setContentView(R.layout.toggle_button_example);
+//        setContentView(R.layout.checkbox_example);
 
 //        addListenerOnButtonExample();
 //        addListenerOnToggleButtonExample();
+//        addListenerOnCheckboxExample();
     }
 
     public void addListenerOnButtonExample ()
@@ -71,6 +80,44 @@ public class MainActivity extends AppCompatActivity {
                 StringBuilder result = new StringBuilder();
                 result.append("Toggle Button1: ").append(toggleButton1.getText());
                 result.append("\nToggle Button2: ").append(toggleButton2.getText());
+
+                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void addListenerOnCheckboxExample()
+    {
+        coffee = (CheckBox) findViewById(R.id.checkBox1);
+        pizza = (CheckBox) findViewById(R.id.checkBox2);
+        burger = (CheckBox) findViewById(R.id.checkBox3);
+        orderButton = (Button) findViewById(R.id.orderButton);
+
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float orderAmount = 0;
+                StringBuilder result = new StringBuilder();
+                result.append("Selected Items:");
+
+                if (coffee.isChecked())
+                {
+                    result.append("\nCoffee: 2,5$");
+                    orderAmount += 2.5;
+                }
+                if (pizza.isChecked())
+                {
+                    result.append("\nPizza: 13$");
+                    orderAmount += 13;
+                }
+                if (burger.isChecked())
+                {
+                    result.append("\nBurger: 12,25$");
+                    orderAmount += 12.25;
+                }
+
+                result.append("\nTotal Order Amount: " + orderAmount + "$");
 
                 Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
             }
